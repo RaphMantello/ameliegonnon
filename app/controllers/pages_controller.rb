bd9e176a76cc2ab0f38a0ce15afe58150e4be92b
raphaelmantello <mantello.raphael@gmail.com>
2020-11-25 11:38:25 +0100

    added home/landing/gallery pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :landing, :gallery ]
 
   def home
+  end
+
+  def landing
+  end
+
+  def gallery
   end
 end
