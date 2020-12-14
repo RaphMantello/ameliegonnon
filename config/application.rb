76e769d1646d79694755cb2e4f9faa8dc12e4639
raphaelmantello <mantello.raphael@gmail.com>
2020-12-14 19:22:13 +0100

    videos folder

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Ameliegonnon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :en
-    config.assets.paths << "#{Rails.root}/app/assets/videos"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
