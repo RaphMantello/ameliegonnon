ec601e2ff20df0df7886abe514c87c2c51ff70fe
raphaelmantello <mantello.raphael@gmail.com>
2020-12-21 17:49:09 +0100

    PressArtocle table created

---
+++ app/models/press_article.rb
@@ -0,0 +1,2 @@
+class PressArticle < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_141113) do
+ActiveRecord::Schema.define(version: 2020_12_21_164736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 2020_12_18_141113) do
   create_table "expositions", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "press_articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
